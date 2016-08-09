6d4a3d33e35da92f0c4128d3bc4218338058e273
QinisoM <QinisoMdletshe@gmail.com>
2016-08-09 21:09:01 +0200

    Improvements as per recommendations Upgraded arrays to js style []

--- getDatabaseList.php
+++ getDatabaseList.php
@@ -1,31 +1,36 @@
-<?php header( 'Content-type:application/json');
-$aFilterOpts = array(
-	'DbHost' => array(
+<?php
+header('Content-Type:application/json');
+$aFilterOpts = [
+	'DbHost' => [
 		'filter' => FILTER_SANITIZE_STRING,
 		'flags' => FILTER_VALIDATE_IP
-	),
+	],
 	'HostUser' => FILTER_SANITIZE_STRING,
 	'HostPass' => FILTER_SANITIZE_STRING,
-);
+];
 $aInput = filter_input_array(INPUT_POST, $aFilterOpts);
 $oOpt = (object)$aInput;
 
 try{
 	$oConn = new PDO('mysql:host=' . $oOpt->DbHost . ';dbname=information_schema', $oOpt->HostUser, (!empty($oOpt->HostPass) ? $oOpt->HostPass : ''));
-} catch (Exception $e) {die;}
-
-// Fatch table info from the info schema
-$aDatabases = [];
-foreach ($oConn->query("
-	SELECT table_schema `Database`
-	FROM information_schema.tables
-	WHERE table_schema" .
-	(!empty($oOpt->DbName)
-		? " IN ('" . implode("','", $oOpt->DbName) . "')"
-		: " NOT IN ('information_schema','performance_schema','mysql','util_replication')"
-	), PDO::FETCH_OBJ) as $oSchemata
-) {
-	$aDatabases[] = $oSchemata->Database;
+} catch (Exception $e) {
+	echo json_encode(['Error' => 'Could not connect to database']);
 }
-$aDatabases = array_unique($aDatabases);
-echo json_encode(array_values($aDatabases));
+
+if ($oConn) {
+	// Fatch table info from the info schema
+	$aDatabases = [];
+	foreach ($oConn->query("
+		SELECT table_schema `Database`
+		FROM information_schema.tables
+		WHERE table_schema" .
+		(!empty($oOpt->DbName)
+			? " IN ('" . implode("','", $oOpt->DbName) . "')"
+			: " NOT IN ('information_schema','performance_schema','mysql','util_replication')"
+		), PDO::FETCH_OBJ) as $oSchemata
+	) {
+		$aDatabases[] = $oSchemata->Database;
+	}
+	$aDatabases = array_unique($aDatabases);
+	echo json_encode(array_values($aDatabases));
+}
